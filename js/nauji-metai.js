6972f347260d4d519e933f2bf9383146b68a8760
Rimantas <belauzas@yahoo.com>
2021-01-08 13:04:43 +0200

    Exercise

--- js/nauji-metai.js
+++ js/nauji-metai.js
@@ -15,3 +15,26 @@ kate.introduce();
 
 console.log(suo.speek());
 suo.introduce();
+
+
+
+
+/*
+
+Animal.js (params: name, color, sound; methods: voice(), introduce();)
+    Pet.js (viska paveldi is Animal.js)
+        Dog.js (viska paveldi is Pet.js)
+        Cat.js (viska paveldi is Pet.js)
+        Hamster.js (viska paveldi is Pet.js; params: canEatTillDeath)
+
+    Bird.js (viska paveldi is Animal.js; params: wingsCount, canFly; methods: flyAway();)
+        Chicken.js (viska paveldi is Bird.js)
+        Parrot.js (viska paveldi is Bird.js)
+        Eagle.js (viska paveldi is Bird.js)
+        Dragonfly.js (viska paveldi is Bird.js; params: imposter=true;)
+
+    Fish.js (viska paveldi is Animal.js; params: canSwim; methods: swimAway();)
+        Goldfish.js (viska paveldi is Fish.js; params: grantsWishes; methods: makeWish();)
+        Nemo.js (viska paveldi is Fish.js)
+
+*/
